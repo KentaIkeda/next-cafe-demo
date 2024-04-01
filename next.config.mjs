59fd1dc405605caf2654f42670835faa22c6cd2a
Kenta Ikeda <77259023+Krudaovo@users.noreply.github.com>
2024-04-01 23:52:14 +0900

    再度修正

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `https://next-cafe-demo-ihj4houna-kenta-ikedas-projects.vercel.app/api/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
